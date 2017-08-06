ab438c1beef25b92a44d3c8522be93d56cc9031b
Carlos Vasquez <cvasquez@ZGMF-X19A.attlocal.net>
2017-08-06 14:40:08 -0400

    Implemented the sounds router

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const player = require('play-sound')(opts = {});
+const express = require('express');
+
+
 
 player.play('./media/minute-remaining.mp3', function (err) {
     if (err) throw err
-})
+    });
